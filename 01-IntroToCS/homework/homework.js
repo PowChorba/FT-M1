f433edd9e297886bc52f5796cf72fd23563eae77
PowChorba <101152834+PowChorba@users.noreply.github.com>
2022-05-24 09:16:32 -0300

    Primer HM

--- 01-IntroToCS/homework/homework.js
+++ 01-IntroToCS/homework/homework.js
@@ -2,12 +2,21 @@
 
 function BinarioADecimal(num) {
   // tu codigo aca
-
+  let suma = 0;
+  for (let i = 0; i < num.length; i++) {
+    suma += num[i] * 2 ** (num.length - 1 - i);
+  }
+  return suma
 }
 
 function DecimalABinario(num) {
   // tu codigo aca
-
+  let string = '';
+  while (num > 0) {
+    string = (num % 2) + string
+    num = Math.floor(num / 2);
+  }
+  return string;
 }
 
 
