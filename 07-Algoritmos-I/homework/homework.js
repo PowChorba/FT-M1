fdaebbbd5e3fc453d1a066af459043076e32e464
PowChorba <101152834+PowChorba@users.noreply.github.com>
2022-06-01 01:26:44 -0300

    Septimo HM

--- 07-Algoritmos-I/homework/homework.js
+++ 07-Algoritmos-I/homework/homework.js
@@ -6,7 +6,18 @@ function factorear(num) {
   // los factores por los cuales se va dividiendo a dicho número (De menor a mayor)
   // Ej: factorear(180) --> [1, 2, 2, 3, 3, 5] Ya que 1x2x2x3x3x5 = 180 y son todos números primos
   // Tu código:
-
+  let random = [1]
+  let divisor = 2
+  while (num != 1){
+    if(num % divisor === 0){
+      random.push(divisor);
+      num = num / divisor;
+      
+    } else {
+      divisor++;
+    }
+  }
+  return random
 }
 
 function bubbleSort(array) {
@@ -14,7 +25,16 @@ function bubbleSort(array) {
   // el array recibido como parámetro
   // Devolver el array ordenado resultante
   // Tu código:
-
+  for(let i = 0; i < array.length; i++){
+    for(let j = 0; j < array.length; j++){
+      if(array[j+1] < array[j]){
+        let nuevo = array[j+1];
+        array[j+1] = array[j]
+        array[j] = nuevo
+      }
+    }
+  }
+  return array;
 }
 
 
@@ -23,8 +43,21 @@ function insertionSort(array) {
   // el array recibido como parámetro utilizando arreglos
   // Devolver el array ordenado resultante
   // Tu código:
+  for(let i = 1; i < array.length; i++){
+    let j = i - 1;
+    let current = array[i]
+    while((j >= 0) && (array[j] > current) ){
+      array[j + 1] = array[j];
+      j--
+    }
+    array[j+1] = current
 
-}
+    }
+    return array;
+  }
+    
+  
+
 
 
 function selectionSort(array) {
@@ -32,7 +65,18 @@ function selectionSort(array) {
   // el array recibido como parámetro utilizando dos arreglos
   // Devolver el array ordenado resultante
   // Tu código:
-
+  for(let i = 0; i < array.length - 1; i++){
+    let minIndex = i;
+    for(let j= i; j < array.length; j++){
+      if(array[j] < array[minIndex]){
+        minIndex = j
+      }
+    }
+    const temp = array[i];
+    array[i] = array[minIndex]
+    array[minIndex] = temp;
+  }
+  return array
 }
 
 
