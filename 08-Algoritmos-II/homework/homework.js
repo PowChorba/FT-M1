bad4e19a57b29dfd401ccf08f2a31dd0825ff17f
PowChorba <101152834+PowChorba@users.noreply.github.com>
2022-06-01 14:30:36 -0300

    Octavo HM

--- 08-Algoritmos-II/homework/homework.js
+++ 08-Algoritmos-II/homework/homework.js
@@ -6,7 +6,22 @@ function quickSort(array) {
   // el array recibido como parámetro
   // Devolver el array ordenado resultante
   // Tu código:
+  if(array.length < 1){
+    return [];
+  }
+  let left = [];
+  let right = [];
+  let num = array[0];
+  let arrFinal = [];
 
+  for(let i = 1; i < array.length; i++){
+    if(array[i] < num){
+      left.push(array[i]);
+    } else {
+      right.push(array[i]);
+    }
+  }
+  return arrFinal.concat(quickSort(left), num, quickSort(right))
 }
 
 function mergeSort(array) {
@@ -14,7 +29,24 @@ function mergeSort(array) {
   // el array recibido como parámetro
   // Devolver el array ordenado resultante
   // Tu código:
+  function merge(left,right){
+    let arr = [];
+    while(left.length && right.length){
+      if(left[0] < right[0]){
+        arr.push(left.shift());
+      } else {
+        arr.push(right.shift());
+      }
+    }
+    return arr.concat(left,right)
+  }
+  const mitad = array.length / 2;
+  if(array.length < 2){
+    return array;
+  }
 
+  let left = array.splice(0, mitad)
+  return merge(mergeSort(left), mergeSort(array))
 }
 
 // No modificar nada debajo de esta línea
